685810bd485bd6a8931d8c5d8bdf84d67417e19e
ManujPorwal <manuj.porwal@gmail.com>
2013-04-17 10:56:12 +0530

    Testing CI tool TeamCity

--- UnitTests/IpAddressTest.m
+++ UnitTests/IpAddressTest.m
@@ -30,7 +30,7 @@
 - (void)testIpAddress
 {
     
-    NSString *address = @"192.168.0.267";
+    NSString *address = @"192.168.0.1";
     
     IpAddress *ipAddress = [[IpAddress alloc] initWithString:address];
     
